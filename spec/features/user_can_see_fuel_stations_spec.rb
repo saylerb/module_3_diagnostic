2e0413569000246a6797d87ae93f614f79c1fe0d
Brian Sayler <sayler.b@gmail.com>
2016-07-25 10:26:12 -0600

    add simple fuel station feature spec

---
+++ spec/features/user_can_see_fuel_stations_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "user can see fuel stations" do
+  scenario "a user navigates to root and sees a list of fuel stations" do
+    visit root_path
+
+    fill_in "Search", with: "80203"
+    click_on "Locate"
+   
+    expect(current_path).to eq("/search?zip=80203")
+
+    expect(page).to have_content("Name: Cultural Center Complex Garage")
+    expect(page).to have_content("Address: 65 W 12th Ave")
+    expect(page).to have_content("Fuel Types: ELEC")
+    expect(page).to have_content("Distance: 0.41801")
+  end
+end
